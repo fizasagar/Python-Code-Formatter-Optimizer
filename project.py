c56e148f271dc00a3aca05876f1d085a5e4769c4
Fiza Sagar <156088954+fizasagar@users.noreply.github.com>
2025-05-04 18:39:05 +0500

    Update project.py

--- project.py
+++ project.py
@@ -2,9 +2,14 @@ from radon.complexity import cc_visit
 from streamlit_lottie import st_lottie 
 
 
+# Function to load animation from a URL
+def load_lottieurl(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
 
-
-animation = load_lottie_url("https://assets10.lottiefiles.com/packages/lf20_j1adxtyb.json")
+lottie_lock = load_lottie_url("https://assets10.lottiefiles.com/packages/lf20_j1adxtyb.json")
 
 
  
